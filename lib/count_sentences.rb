1df3d0ef4c450b7f8297a9d0d82ba4c12c45c937
Ben Hogan <j.benhogan@gmail.com>
2020-06-23 21:46:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     count = 0 
-    puts self.split(".").split("!").split("?")
+    puts self.split(".").count
     puts self.split("!").count
     puts self.split("?").count
   end
